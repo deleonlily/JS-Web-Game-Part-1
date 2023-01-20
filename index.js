8ab16dfc35f9b6f2bd6f872d62456078b2fea7e2
Lilian de Leon <deleonarleth17@gmail.com>
2023-01-20 01:47:20 -0500

    changes

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-function newImage() {
-    greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
-    return greenCharacter    
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
-
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
-
-sword.addEventListener('click', function() {
-    sword.remove()
-})
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
